613ea06c00591ed3fbbb8ab814b2838d537b9a36
Yao Chen <yaochen2@student.unimelb.edu.au>
2025-09-19 22:28:41 +1000

    add click to move script to test

--- Assets/Scripts/PlayerController.cs
+++ Assets/Scripts/PlayerController.cs
@@ -1,16 +1,69 @@
 using UnityEngine;
+using UnityEngine.AI;
+using UnityEngine.EventSystems;
+using UnityEngine.InputSystem;
 
+[RequireComponent(typeof(NavMeshAgent))]
 public class PlayerController : MonoBehaviour
 {
-    // Start is called once before the first execution of Update after the MonoBehaviour is created
-    void Start()
+    public LayerMask groundMask;
+    public float maxRayDistance = 1000f;
+
+    private NavMeshAgent agent;
+    private TestinputTemp input;
+    private Camera cam;
+
+    void Awake()
     {
-        
+        agent = GetComponent<NavMeshAgent>();
+        cam = Camera.main;
+        input = new TestinputTemp();
     }
 
-    // Update is called once per frame
+    void OnEnable()
+    {
+        input.Enable();
+    }
+
+    void OnDisable()
+    {
+        input.Disable();
+    }
+
     void Update()
     {
-        
+        HandleClick();
+    }
+
+    private void HandleClick()
+    {
+        if (!input.PlayerMove.Move.WasPerformedThisFrame())
+        {
+            return;
+        }
+
+        if (EventSystem.current != null && EventSystem.current.IsPointerOverGameObject())
+        {
+            return;
+        }
+
+        Vector2 screenPos = input.PlayerMove.GetPoint.ReadValue<Vector2>();
+
+        if (cam == null)
+        {
+            cam = Camera.main;
+            if (cam == null)
+            {
+                return;
+            }
+        }
+
+        Ray ray = cam.ScreenPointToRay(screenPos);
+
+        RaycastHit hit;
+        if (Physics.Raycast(ray, out hit, maxRayDistance, groundMask))
+        {
+            agent.SetDestination(hit.point);
+        }
     }
 }
