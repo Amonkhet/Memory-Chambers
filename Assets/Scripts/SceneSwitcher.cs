fd8703081fd44cb3501530fd4a078b3ec045aba6
Yao Chen <yaochen2@student.unimelb.edu.au>
2025-10-12 01:58:16 +1100

    Update enter door listener

--- Assets/Scripts/SceneSwitcher.cs
+++ Assets/Scripts/SceneSwitcher.cs
@@ -5,6 +5,16 @@ public class SceneSwitcher : MonoBehaviour
     // Event listner
     void OnEnable()
     {
-        
+        EventManager.OnDoorEnter += ActivateDoor;
+    }
+
+    void OnDisable()
+    {
+        EventManager.OnDoorEnter += ActivateDoor;
+    }
+    // Listen to event
+    void ActivateDoor(EventManager.DoorToScene door)
+    {
+        SceneManager.LoadScene(door.targetScene);
     }
 }
