a09915df8c20c3ebb358f80df74dd2995d1af014
Yao Chen <yaochen2@student.unimelb.edu.au>
2025-10-13 02:15:37 +1100

    create game status script to manage game object status

---
+++ Assets/Scripts/BookDropActivate.cs
@@ -0,0 +1,23 @@
+using Unity.AI.Navigation;
+using UnityEngine;
+using UnityEngine.Playables;
+
+public class BookActivate : MonoBehaviour
+{
+    // Attributes
+    [Header("Animation and NavMeshLink")]
+    public PlayableDirector playableDirector;
+    public NavMeshLink navMeshLink;
+    private bool hasTriggered = false;
+    // Start is called once before the first execution of Update after the MonoBehaviour is created
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
