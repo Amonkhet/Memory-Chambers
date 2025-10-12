3daf5b7fa22673707151430de935a66334bf0af8
Yao Chen <yaochen2@student.unimelb.edu.au>
2025-10-13 02:38:57 +1100

    Add book drop activate mechanics allow click to activate

--- Assets/Scripts/BookDropActivate.cs
+++ Assets/Scripts/BookDropActivate.cs
@@ -8,16 +8,61 @@ public class BookActivate : MonoBehaviour
     [Header("Animation and NavMeshLink")]
     public PlayableDirector playableDirector;
     public NavMeshLink navMeshLink;
+    [Header("Mechanics key id")]
+    [SerializeField] private string mechanicKey = "BookDrop";
     private bool hasTriggered = false;
     // Start is called once before the first execution of Update after the MonoBehaviour is created
     void Start()
     {
-        
+        // If this mechanics activated, reset
+        if (GameStatus.IsActivated(mechanicKey))
+        {
+            ResetDoorStatus();
+        }
+        else
+        {
+            SetDoorStatus();
+        }
     }
 
-    // Update is called once per frame
-    void Update()
+    public void ActivateDoor()
     {
-        
+        // Play timeline animation
+        if (playableDirector)
+        {
+            playableDirector.time = 0;
+            playableDirector.Play();
+        }
+        // Activate navmeshlink
+        if (navMeshLink)
+        {
+            navMeshLink.enabled = true;
+        }
+        hasTriggered = true;
+        GameStatus.Activated(mechanicKey, true);
+    }
+    // Make sure when door activated, it will keep the dropped status even when change scene
+    private void ResetDoorStatus()
+    {
+        if (playableDirector)
+        {
+            playableDirector.time = playableDirector.duration;
+            playableDirector.Evaluate();
+        }
+
+        if (navMeshLink)
+        {
+            navMeshLink.enabled = true;
+        }
+        hasTriggered = true;
+    }
+    // Make sure if the door is never activated, set it to default
+    private void SetDoorStatus()
+    {
+        if (navMeshLink)
+        {
+            navMeshLink.enabled = false;
+        }
+        hasTriggered = false;
     }
 }
