f13df0aef8fc3322c898c94cc2982aa8fb2de4af
Yao Chen <yaochen2@student.unimelb.edu.au>
2025-10-12 23:11:38 +1100

    Update gamemanager to fix bug on webgl, rebuilt webgl for testing

--- Assets/Scripts/GameManager.cs
+++ Assets/Scripts/GameManager.cs
@@ -1,5 +1,9 @@
+using System.Collections;
+using Unity.VisualScripting;
 using UnityEngine;
 using UnityEngine.AI;
+using UnityEngine.SceneManagement;
+
 public class GameManager : MonoBehaviour
 {
     // Attributes
@@ -9,19 +13,87 @@ public class GameManager : MonoBehaviour
     public string defaultScale = "S";
     public Transform spawnPointRoot;
     public string defaultSpawnPoint = "SpawnS";
-
+    // Subscribe to event
+    void OnEnable()
+    {
+        SceneManager.sceneLoaded += OnSceneLoaded;
+    }
+    // Find what scene is loaded
+    void OnSceneLoaded(Scene scene, LoadSceneMode mode)
+    {
+        if (scene == gameObject.scene)
+        {
+            StartCoroutine(LocateToSpawn());
+        }
+    }
     void Start()
     {
+        // Make sure the script is activated in this loaded scene
+        if (gameObject.scene == SceneManager.GetActiveScene())
+        {
+            StartCoroutine((LocateToSpawn()));
+        }
+    }
+    // Wait a few frame and locate player model to corresponding spawn point
+    IEnumerator LocateToSpawn()
+    {
+        NavMeshAgent agent;
+        if (playerSwitcher != null)
+        {
+            agent = playerSwitcher.agent;
+        }
+        else
+        {
+            agent = null;
+        }
+        if (agent != null) { agent.isStopped = true; agent.ResetPath(); }
+        // When trun to webgl build, wait three frame to allow webgl load completely
+#if UNITY_WEBGL
+        for (int i = 0; i < 3; i++)
+        {
+            yield return null;
+        }  
+#else
+        yield return null;                              
+#endif
         string scale = SceneSwitchData.HasData ? SceneSwitchData.ScaleId    : defaultScale;
         string spawn = SceneSwitchData.HasData ? SceneSwitchData.SpawnPoint : defaultSpawnPoint;
-        if (SceneSwitchData.HasData)
+        // Find spawn point
+        Transform spawnPoint = null;
+        if (spawnPointRoot)
         {
-            SceneSwitchData.ClearData();
+            if (!string.IsNullOrEmpty(spawn))
+            {
+                spawnPoint = spawnPointRoot.Find(spawn);
+            }
+            if (spawnPoint == null && !string.IsNullOrEmpty(defaultSpawnPoint))
+            {
+                spawnPoint = spawnPointRoot.Find(defaultSpawnPoint);
+            }
         }
-        // Set player to spawn position
-        Transform spawnPoint = spawnPointRoot.Find(spawn);
-        playerSwitcher.transform.position = spawnPoint.position;
+        // Disable nav mesh agent before transform and change spawn point
+        bool agentEnabled = agent != null && agent.enabled;
+        if (agentEnabled) agent.enabled = false;
+
+        if (spawnPoint)
+        {
+            playerSwitcher.transform.SetPositionAndRotation(spawnPoint.position, spawnPoint.rotation);
+        }
         playerSwitcher.SwitchPlayer(scale);
+        // Add one frame load time for scene to switch player and attributes
+#if UNITY_WEBGL
+        yield return null; 
+#endif
+
+        if (agentEnabled) agent.enabled = true;
+
+        if (agent != null)
+        {
+            agent.Warp(playerSwitcher.transform.position);
+            agent.ResetPath();
+            agent.isStopped = false;
+        }
+
+        if (SceneSwitchData.HasData) SceneSwitchData.ClearData();
     }
-    
 }
