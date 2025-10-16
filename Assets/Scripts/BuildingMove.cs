495b19c6f928dc61f3404f85be0cc7c43c86d77d
Yao Chen <yaochen2@student.unimelb.edu.au>
2025-10-16 21:17:13 +1100

    debug

--- Assets/Scripts/BuildingMove.cs
+++ Assets/Scripts/BuildingMove.cs
@@ -11,7 +11,7 @@ public class BuildingMove : MonoBehaviour
 
     private void Update()
     {
-        if (Input.GetMouseButtonDown(1))
+        if (Input.GetMouseButtonDown(0))
         {
             Ray ray = Camera.main.ScreenPointToRay(Input.mousePosition);
             if (Physics.Raycast(ray, out RaycastHit hit) && hit.collider.gameObject == gameObject)
