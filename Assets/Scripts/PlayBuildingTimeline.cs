5b5f14c9040c16cfd2edca251ae1f39c4f703a5e
Yao Chen <yaochen2@student.unimelb.edu.au>
2025-10-22 16:15:59 +1100

    Update PlayBuildingTimeline.cs

--- Assets/Scripts/PlayBuildingTimeline.cs
+++ Assets/Scripts/PlayBuildingTimeline.cs
@@ -1,15 +1,52 @@
 using UnityEngine;
+using UnityEngine.EventSystems;
 using UnityEngine.Playables;
 
 public class PlayBuildingTimeline : MonoBehaviour
 {
-    [Header("Timelines")]
-    public PlayableDirector timelineRise;
-    public PlayableDirector timelineFall;
+    [Header("Timeline manager")] 
+    public BuildingTimelineManager manager;
 
-    public bool risePlayEnd = false;
-    public bool fallPlayEnd = false;
-    private bool isPlaying;
-    // Control whether to play time forward or backward
+    [Header("TimelineRise or Fall")] 
+    public bool playRiseTimeline = true;
     
+    [Header("Play direction")]
+    public bool playBackward = false;
+    
+    [Header("Activate key")]
+    public KeyCode activateKey = KeyCode.Mouse1;
+    public LayerMask clickMask = ~0;
+
+    void Update()
+    {
+        if (Input.GetKeyDown(activateKey))
+        {
+            var ray = Camera.main.ScreenPointToRay(Input.mousePosition);
+            if (Physics.Raycast(ray, out var hit, 100f, clickMask))
+            {
+                if (hit.collider && (hit.collider.transform == transform || hit.collider.transform.IsChildOf(transform)))
+                {
+                    PlayTimeline();
+                }
+            }
+        }
+    }
+
+    void PlayTimeline()
+    {
+        if (!manager)
+        {
+            return;
+        }
+
+        if (playRiseTimeline)
+        {
+            manager.PlayTimelineRise(playBackward);
+        }
+        else
+        {
+            manager.PlayTimelineFall(playBackward);
+        }
+        
+    }
 }
