6e48e9061b6b4c6d46b7bf35f672e2b2fa032fb7
Yao Chen <yaochen2@student.unimelb.edu.au>
2025-10-25 19:30:13 +1100

    Add script to trigger timeline in outworld

---
+++ Assets/Scripts/LeverActivate.cs
@@ -0,0 +1,47 @@
+using UnityEngine;
+using UnityEngine.Playables;
+public class LeverActivate : MonoBehaviour
+{
+    [Header("Timeline Settings")]
+    [SerializeField] private PlayableDirector timeline; 
+
+    [Header("Interaction Settings")]
+    [SerializeField] private string playerTag = "Player"; 
+    [SerializeField] private KeyCode activateKey = KeyCode.Mouse1;
+    private bool playerInRange = false;
+    private bool activated = false;
+    // Trigger when player in range
+    private void OnTriggerEnter(Collider other)
+    {
+        if (other.CompareTag(playerTag))
+        {
+            playerInRange = true;
+        }
+    }
+
+    private void OnTriggerExit(Collider other)
+    {
+        if (other.CompareTag(playerTag))
+        {
+            playerInRange = false;
+        }
+    }
+
+    private void Update()
+    {
+        if (playerInRange && !activated && Input.GetKeyDown(activateKey))
+        {
+            activated = true;
+            PlayTimeline();
+        }
+    }
+    // Play timeline animation
+    private void PlayTimeline()
+    {
+        if (timeline == null)
+        {
+            return;
+        }
+        timeline.Play();
+    }
+}
