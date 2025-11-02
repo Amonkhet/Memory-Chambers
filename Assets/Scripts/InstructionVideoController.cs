26f134d47cc1761b569794cdea4fb94410e84f7a
jolylin620 <zizlin@student.unimelb.edu.au>
2025-11-03 03:04:21 +1100

    update instruction video

---
+++ Assets/Scripts/InstructionVideoController.cs
@@ -0,0 +1,34 @@
+using UnityEngine;
+using UnityEngine.Video;
+
+public class InstructionVideoController : MonoBehaviour
+{
+    public VideoPlayer videoPlayer;
+    public GameObject videoUI; // The Raw Image panel
+
+    public void PlayInstructionVideo()
+    {
+        if (videoPlayer != null && videoUI != null)
+        {
+            videoUI.SetActive(true);
+            videoPlayer.Play();
+            videoPlayer.loopPointReached += OnVideoEnd;
+        }
+    }
+
+    void OnVideoEnd(VideoPlayer vp)
+    {
+        videoUI.SetActive(false);
+    }
+
+    // Optional: Add a method to close manually
+    public void CloseVideo()
+    {
+        if (videoPlayer != null)
+        {
+            videoPlayer.Stop();
+            videoUI.SetActive(false);
+        }
+    }
+}
+
