577be5419753b0dd09eabd61f53f59cbf59da488
RyanWang688 <ywwa15@student.unimelb.edu.au>
2025-11-02 18:39:06 +1100

    added quit game
    added quit game

---
+++ Assets/Scripts/QuitGame.cs
@@ -0,0 +1,17 @@
+using UnityEngine;
+
+public class QuitGame : MonoBehaviour
+{
+    public void Quit()
+    {
+        Debug.Log("Game is quitting...");  
+
+#if UNITY_EDITOR
+
+        UnityEditor.EditorApplication.isPlaying = false;
+#else
+
+        Application.Quit();
+#endif
+    }
+}
