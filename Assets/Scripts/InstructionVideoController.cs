71d0999226bd4c971b55135b074f8b1a821e134e
jolylin620 <zizlin@student.unimelb.edu.au>
2025-11-04 12:04:59 +1100

    change video files

--- Assets/Scripts/InstructionVideoController.cs
+++ Assets/Scripts/InstructionVideoController.cs
@@ -6,7 +6,15 @@ public class InstructionVideoController : MonoBehaviour
 {
     public VideoPlayer videoPlayer;
     public GameObject videoUI; // The Raw Image panel
-
+    public string videoFileName = "instruction.mp4";
+    void Start()
+    {
+#if UNITY_WEBGL
+        // Load video from StreamingAssets path
+        string videoPath = System.IO.Path.Combine(Application.VideosPath, videoFileName);
+        videoPlayer.url = videoPath;
+#endif
+    }
     public void PlayInstructionVideo()
     {
         if (videoPlayer != null && videoUI != null)
@@ -33,6 +41,7 @@ public class InstructionVideoController : MonoBehaviour
 
     public void OnPointerClick(PointerEventData eventData)
     {
+        Debug.Log("Video panel clicked!");
         CloseVideo();
     }
 }
